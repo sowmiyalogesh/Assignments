8a699c8a05d8043887f7b43f520084899986ab35
sowmiyalogesh <127213464+sowmiyalogesh@users.noreply.github.com>
2023-03-07 18:58:05 +0530

    Add files via upload

---
+++ day1/DataType.java
@@ -0,0 +1,21 @@
+package week1.day1;
+
+public class DataType {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//Today class Details
+		int classno=1;
+		char classagneda='J';
+		String topic="Java";
+		float hoursofclass=4.30f;
+		long mob=918879898l;
+		double date=0403.2023;
+		boolean classdone=true;
+		System.out.println("Today Class details"+" "+classno+" "+ hoursofclass);
+		
+		
+				
+	}
+
+}
